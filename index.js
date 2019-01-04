34c57481f1d2105b0bf920c338f4ffcf3d1dd9f6
Matthew Hilbert <matthewhilbert@gmail.com>
2019-01-03 20:37:37 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const Discord = require("discord.js");
+const client = new Discord.Client();
+ 
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+ 
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
+ 
+client.login(process.env.BOT_TOKEN);
