3352be22ae0521883d92191d3b3e110a9583d1ce
Matthew Hilbert <matthewhilbert@gmail.com>
2019-01-05 12:44:26 -0500

    Improve ping

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-require('dotenv').config()
-const Discord = require("discord.js");
+require('dotenv').config();
+const Discord = require('discord.js');
+
 const client = new Discord.Client();
- 
-client.on("ready", () => {
-  console.log("I am ready!");
+
+client.on('ready', () => {
+  console.log('I am ready!');
 });
- 
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+
+client.on('message', (message) => {
+  if (message.author.bot) return; // Ignore bot messages
+  const args = message.content.split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'ping') {
+    message.channel.send('pong...').then((msg) => {
+      msg.edit(`pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}`);
+    });
   }
 });
- 
+
 client.login(process.env.BOT_TOKEN);
