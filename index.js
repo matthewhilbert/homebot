dc54a307c84dfe4e5fe991d8dc58143bbe54b857
Matthew Hilbert <matthewhilbert@gmail.com>
2019-01-05 13:14:15 -0500

    Add prettier eslint config/plugin

--- index.js
+++ index.js
@@ -7,14 +7,18 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
-client.on('message', (message) => {
+client.on('message', message => {
   if (message.author.bot) return; // Ignore bot messages
   const args = message.content.split(/ +/g);
   const command = args.shift().toLowerCase();
 
   if (command === 'ping') {
-    message.channel.send('pong...').then((msg) => {
-      msg.edit(`pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}`);
+    message.channel.send('pong...').then(msg => {
+      msg.edit(
+        `pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(
+          client.ping
+        )}`
+      );
     });
   }
 });
